d0d9d3c1c4bd69726b7306c605752ee4874de5b6
McAuley, Frank <Frank.McAuley@alliancedata.com>
2020-02-06 14:42:44 -0500

    FM - Chapter2 working on the basics

--- src/Chapter1.kt
+++ src/Chapter1.kt
@@ -2,6 +2,16 @@ fun main() {
     val persons = listOf(Person("Alice"), Person("Bob", age = 29))
     val oldest = persons.maxBy { it.age ?: 0} //can't have a null value
     println("The oldest is: $oldest")
+
+    println(max(3,4))
 }
+
+fun max(a: Int, b: Int): Int {
+    return if (a > b) a else b
+}
+
+// Statement vs Expression
+// expression has a value, which can be used as part of another expression, whereas a statement
+// is always a top-level element in its enclosing block and does not have a value of it's own.
 
 data class Person(val name: String, val age: Int? = null)
