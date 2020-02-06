5ddfb94683b9ddcd19933b12ffc441d094183164
McAuley, Frank <Frank.McAuley@alliancedata.com>
2020-02-06 16:45:40 -0500

    FM - enum example with function and constructor also the only place in kotlin where you use a ; semi colon

--- src/Chapter1.kt
+++ src/Chapter1.kt
@@ -14,6 +14,9 @@ fun main(args: Array<String>) {
 
     println("Hello hello hello, ${if (args.size > 0) args[0] else "Someone!"}")
 
+
+    println(Color.BLUE.rgb())
+
 }
 
 fun max(a: Int, b: Int): Int {
@@ -35,6 +38,10 @@ class Rectangle(val height: Int, val width: Int) {
     }
 }
 
-enum class Color {
-    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+enum class Color (val r: Int, val g: Int, val b: Int) {
+    RED(255,0,0), ORANGE(255, 165, 0), YELLOW(255,255,0),
+    GREEN(0,255,0), BLUE(0,0,255),
+    INDIGO(75,0,130), VIOLET(238,130,238);
+
+    fun rgb() = (r * 256 + g) * 256 + b
 }
